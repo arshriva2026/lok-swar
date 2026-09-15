3de72f2a34b2100244151872c96bf8c13d6b1b3c
aman-kumar-singh-20022006 <amansingh20022006@gmail.com>
2026-09-15 18:20:21 +0530

    UI redesign: Human-made aesthetic and removed unused previews

--- ZeroLiteracyHome.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LokSwarInteractive from './LokSwarInteractive';
-
-export default function ZeroLiteracyHome(props) {
-  return <LokSwarInteractive {...props} />;
-}
